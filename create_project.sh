dc83fbb26294273e83653592dd43d5fca4bbf6e6
Yevhenii Bilyk <blinkme1@ukr.net>
2017-02-02 15:48:47 +0200

    refactoring

--- create_project.sh
+++ create_project.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -rf ./img
+rm -rf ./img/*
 rm -rf ./_data
 rm -rf ./_pages
 
 cp -rf ./proj-data/_data .
 cp -rf ./proj-data/_pages .
-cp -rf ./proj-data/img .
+cp -rf ./proj-data/images/* ./img
 
 cp -rf ./wp-data/img .
 cp -rf ./wp-data/_data .

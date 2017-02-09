6fa9f08a06efd5829eb9eea8cdd71ad1ff694505
Yevhenii Bilyk <blinkme1@ukr.net>
2017-02-09 11:04:37 +0200

    small fix

--- run.sh
+++ run.sh
@@ -21,10 +21,12 @@ rm -rf ./img/*
 rm -rf ./_data
 rm -rf ./_pages
 
+mkdir -p img
+
 cp -rf ./proj-data/cache.yml ./_assets-cache
 cp -rf ./proj-data/_data .
 cp -rf ./proj-data/_pages .
-cp -rf ./proj-data/images/* ./img
+cp -rf ./proj-data/images/* ./img/
 
 cp -rf ./wp-data/_posts .
 cp -rf ./wp-data/img .

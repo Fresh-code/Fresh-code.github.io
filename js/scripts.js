a51f9704e0c002bc81568cb9b19730d623066819
Yevhenii Bilyk <blinkme1@ukr.net>
2017-03-20 09:56:50 +0200

    update site 20/03/17 09:56:50

--- js/scripts.js
+++ js/scripts.js
@@ -5,74 +5,71 @@
  * @depends libraries/parsley.min.js
  * @depends libraries/prism.min.js
  */
-(function(global) {
+(function (global) {
 
-    $(window).on("load", function() {
+    $(window).on("load", function () {
 
-        $.fn.randomize = function(selector){
-            var $elems = selector ? $(this).find(selector) : $(this).children(),
-                $parents = $elems.parent();
-            $parents.each(function(){
-                $(this).children(selector).sort(function(){
+        $.fn.random = function (selector) {
+            var $elements = selector ? $(this).find(selector) : $(this).children(),
+                $parents = $elements.parent();
+            $parents.each(function () {
+                $(this).children(selector).sort(function () {
                     return Math.round(Math.random()) - 0.5;
                 }).detach().appendTo(this);
             });
-            $( "#testimonials .testimonial").each( function( index, element) {
-                if(index % 2){
-                    $(element).addClass("swap-hr");
-                }
-            });
-            $('#testimonials').css({'display':'block'});
+        };
+
+        $.fn.randomTestimonials = function (selector) {
+            $(this).random(selector);
+            if (this.selector == '#testimonials-main') { //logic on the main page
+                $(this).find(selector).each(function (index, element) {
+                    if (index < 2) {
+                        $(element).css({'display': 'block'});
+                    }
+                });
+            } else { //logic on the testimonials page
+                $(this).find(".testimonial").each(function (index, element) {
+                    if (index % 2) {
+                        $(element).addClass("swap-hr");
+                    }
+                });
+                $(this).css({'display': 'block'});
+            }
             return this;
         };
-        $('#testimonials').randomize('.testimonial-block');
 
+        $('#testimonials').randomTestimonials('.testimonial-block');
+        $('#testimonials-main').randomTestimonials('.testimonial-main');
 
-        $.fn.mainTestimonialsRandomize = function(selector){
-            var $elems = selector ? $(this).find(selector) : $(this).children(),
-                $parents = $elems.parent();
-            $parents.each(function(){
-                $(this).children(selector).sort(function(){
-                    return Math.round(Math.random()) - 0.5;
-                }).detach().appendTo(this);
-            });
-            $( "#testimonials-main .testimonial-main").each( function( index, element) {
-                if((index == 0 )|| (index == 1)){
-                    $(element).css({'display':'block'});
-                }
-            });
-            return this;
-        };
-        $('#testimonials-main').mainTestimonialsRandomize('.testimonial-main');
 
         var byMenuBtn = false,
             byMailBtn = false,
             byThx = false,
             closeMailForm = false;
 
-        $('.hire').on('click', function(e) {
+        $('.hire').on('click', function (e) {
             e.preventDefault();
 
             byMailBtn = true;
             $('.icon-sign').trigger('click');
         });
 
-        $('.hamburger').on('click', function(e) {
+        $('.hamburger').on('click', function (e) {
             if (!byMailBtn) {
                 if (byMenuBtn) {
-                        $('.hamburger').removeClass('close-nav');
-                        $('.icon-sign').removeClass('hidden');
-                        setTimeout(function() {
-                            $('body').removeClass('menu-opened');
-                            $('body').removeClass('form-opened');
-                            $('body').removeClass('locked');
-                        }, 150);
-                        byMenuBtn = false;
+                    $('.hamburger').removeClass('close-nav');
+                    $('.icon-sign').removeClass('hidden');
+                    setTimeout(function () {
+                        $('body').removeClass('menu-opened');
+                        $('body').removeClass('form-opened');
+                        $('body').removeClass('locked');
+                    }, 150);
+                    byMenuBtn = false;
                 } else {
-                    if(byThx) thxClose(true);
+                    if (byThx) thxClose(true);
 
                     $('.hamburger').addClass('close-nav');
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $('body').addClass('menu-opened');
                     }, 150);
                     $('.icon-sign').addClass('hidden');
@@ -82,11 +79,11 @@
             } else {
                 closeMailForm = false;
                 byMailBtn = false;
-                if(byThx) thxClose(false);
+                if (byThx) thxClose(false);
 
                 $('.hamburger').removeClass('close-nav');
                 $('.icon-sign').removeClass('hidden');
-                setTimeout(function() {
+                setTimeout(function () {
                     $('body').removeClass('locked');
                 }, 150);
                 $('body')
@@ -95,13 +92,13 @@
             }
         }); // Click on menu icon
 
-        $('.icon-sign').on('click', function() {
-            if(!byMailBtn) {
+        $('.icon-sign').on('click', function () {
+            if (!byMailBtn) {
 
-                if(byThx) thxClose(false);
+                if (byThx) thxClose(false);
 
                 byMailBtn = true;
-                setTimeout(function() {
+                setTimeout(function () {
                     $('body').addClass('form-opened');
                 }, 150);
                 $('.hamburger').addClass('close-nav');
@@ -118,7 +115,7 @@
 
             $('body')
                 .removeClass('thx-opened')
-                .removeClass( flag ? 'form-opened' : 'menu-opened');
+                .removeClass(flag ? 'form-opened' : 'menu-opened');
 
             $('#one').css('display', 'flex');
             $('#two').css('display', 'none');
@@ -141,7 +138,9 @@
             resetForm();
         }
 
-        function resetForm() { $form[0].reset(); }
+        function resetForm() {
+            $form[0].reset();
+        }
 
         var $form = $('form.hire-us-form'),
             $name = $form.find("input[name='name']"),
@@ -153,27 +152,6 @@
         $form.submit(function (e) {
             e.preventDefault();
 
-            /*$.ajax({
-                method: "POST",
-                //url: "https://getform.org/f/07683c3f-c72c-47bc-98d7-2f0e866fab3f",
-                url: "https://freshcodeit.com",
-                data: {
-                    name: $name.val(),
-                    email: $email.val(),
-                    subject: $subject.val(),
-                    message: $message.val()
-                },
-                crossDomain: true,
-
-                success: function(){
-                    thx();
-                },
-                error: function(XMLHttpRequest, textStatus, errorThrown) {
-                    alert("Status: " + textStatus); alert("Error: " + errorThrown);
-                }
-            });*/
-
-
             $.ajax({
                 method: "POST",
                 url: "https://getform.org/f/07683c3f-c72c-47bc-98d7-2f0e866fab3f",
@@ -184,18 +162,24 @@
                     message: $message.val()
                 },
                 crossDomain: true,
+                error: function (xhr){
+                    if(xhr.status==404 || xhr.status==500) {
+                        _gaq.push(['_trackEvent','button', 'getform', 'error'])
+                    }
+                },
             });
 
             thx();
             $form[0].reset();
         });
 
+
         menuOnScroll();
     }); //$(window).on("load", function()...
 
 
     var lastScrollTop = 0;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         menuOnScroll();
     });
 
@@ -207,27 +191,28 @@
         if ($(window).width() > 1000) {
 
             $('.parallax-text-blog').css({
-                "transform" : "translate3d(0px, " + st/6 + "px, 0px)"
+                "transform": "translate3d(0px, " + st / 6 + "px, 0px)"
             });
             $('.paralax-text').css({
-                "transform" : "translate3d(0px, " + st * -0.5 + "px, 0px)",
-                "opacity" : 1 - st/500
+                "transform": "translate3d(0px, " + st * -0.3 + "px, 0px)",
+              //  "opacity": 1 - st / 900
             });
             $('.parallax-text').css({
-                "transform" : "translate3d(0px, " + st/25 + "px, 0px)"
+                "transform": "translate3d(0px, " + st / 25 + "px, 0px)"
             });
             $('.parallax-section').css({
-                "transform" : "translate3d(0px, " + st/6 + "px, 0px)"
+                "transform": "translate3d(0px, " + st / 6 + "px, 0px)"
             });
         }
-
 
 
         if (st < 1) {
             header.removeClass('is-scrolled');
             header.removeClass('scrolled-by');
         }
-        else if (st >= lastScrollTop) { header.addClass('is-scrolled'); }
+        else if (st >= lastScrollTop) {
+            header.addClass('is-scrolled');
+        }
         else {
             header.addClass('scrolled-by');
             header.removeClass('is-scrolled');

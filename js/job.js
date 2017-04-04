7b8e95a0081f750326250fec5f16da06837e7e0d
Nikita <sirmonin@gmail.com>
2017-04-04 15:58:50 +0300

    small fixes

--- js/job.js
+++ js/job.js
@@ -18,17 +18,16 @@
     $form.submit(function (e) {
         e.preventDefault();
 
+        var data = new FormData();
+        data.append('text', $form.find("textarea").val());
+        data.append('file', $('#file')[0].files[0]);
+
         $.ajax({
             method: "POST",
-            url: "https://docs.google.com/forms/d/e/1FAIpQLSc37CcojHy91KU2tuQ1aTHBy7vw6JaWK5IqfIvSoPnDWH0cWw/formResponse",
-            data: {
-                "entry.1916953323": $form.find('[name="name"]').val(),
-                "entry.321153308": $form.find('[name="email"]').val(),
-                "entry.1961831271": "'"+$form.find('[name="phone"]').val(),
-                "entry.694970312": $form.find('[name="about"]').val()
-
-            },
-            dataType: "jsonp",
+            url: "https://getform.org/u/470a1fb1-94b3-4094-a7d7-565f28f0c877",
+            data: data,
+            processData: false,
+            contentType: false,
             crossDomain: true
         }).done(function(res) {
             console.log(res);
@@ -38,4 +37,3 @@
 //        setTimeout(window.location = '/', 5000);
     });
 })(this);
-

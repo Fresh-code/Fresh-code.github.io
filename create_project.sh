9a168cecdd94d0b2f7340f63a97a9057a56fea3f
Nikita <sirmonin@gmail.com>
2017-04-06 11:36:26 +0300

    Fixed structural outline

--- create_project.sh
+++ create_project.sh
@@ -4,17 +4,12 @@
 #git pull
 #cd ..
 
-rm -rf ./img/*
 rm -rf ./_data
 rm -rf ./_pages
 rm -rf ./_posts
 
-mkdir -p img
-
-cp -rf ./proj-data/cache.yml ./_assets-cache
 cp -rf ./proj-data/_data .
 cp -rf ./proj-data/_pages .
-cp -rf ./proj-data/images/* ./img/
 
 cp -rf ./wp-data/img .
 cp -rf ./wp-data/_data .

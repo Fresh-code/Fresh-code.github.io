b4e98c231cd910dfa6d5c13482d306a12019afa0
Nikita <sirmonin@gmail.com>
2017-04-14 16:18:59 +0300

    Forms update

--- js/job.js
+++ js/job.js
@@ -5,6 +5,7 @@
 (function(global) {
 
     var $menu=false;
+    var $focused=false;
 
     $('textarea.material-input').autogrow({ horizontal: false, flickering: false});
     $(document).ready(function(){
@@ -37,16 +38,17 @@
 
 //        setTimeout(window.location = '/', 5000);
     });
+
     $(document).click(function() {
         $menu = false;
         $(".fresh-select-list").css("opacity", "");
         $(".fresh-select-list").css("visibility", "");
-
     });
 
     $('div.fresh-select').click(function (e) {
         e.stopPropagation();
         if (!$menu) {
+
             $menu = true;
             $(".fresh-select-list").css("opacity", "1");
             $(".fresh-select-list").css("visibility", "visible");
@@ -56,9 +58,13 @@
             $(".fresh-select-list").css("visibility", "");
         }
     });
-    $(".fresh-select-list li").click(function () {
+
+    $(".fresh-select-list li").on("mouseup mousedown", function () {
         $(".fresh-select-input").val($(this).find("span").text());
         $(".fresh-select-input").parsley().validate();
+    });
 
+    $(".fresh-select-input").on("paste change keypress", function () {
+        return false;
     });
 })(this);

695de5b20990ffa736ada1fa6561837d84f0a2ed
Nikita <sirmonin@gmail.com>
2017-04-14 18:01:34 +0300

    14/04/17 18:01:34

--- build.sh
+++ build.sh
@@ -9,17 +9,17 @@ git commit -m "`date +"%d/%m/%y %H:%M:%S "`"
 git push origin dev
 
 #Generate site with last update
-#jekyll build
-#cd _site/
+jekyll build
+cd _site/
 
 # Commit and push last update to master branch
-#git add .
-#git commit -m "update site `date +"%d/%m/%y %H:%M:%S "`"
-#git push origin master
+git add .
+git commit -m "update site `date +"%d/%m/%y %H:%M:%S "`"
+git push origin master
 
 # Purge all files in cloudflare
-#curl -X DELETE "https://api.cloudflare.com/client/v4/zones/1c7a4eedbccff910e14ae06cc90a3622/purge_cache" \
-#-H "X-Auth-Email: social.freshcode@gmail.com" \
-#-H "X-Auth-Key: 8c763444dc7db4df9f52fd51f939e0a9d8eed" \
-#-H "Content-Type: application/json" \
-#--data '{"purge_everything":true}'
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/1c7a4eedbccff910e14ae06cc90a3622/purge_cache" \
+-H "X-Auth-Email: social.freshcode@gmail.com" \
+-H "X-Auth-Key: 8c763444dc7db4df9f52fd51f939e0a9d8eed" \
+-H "Content-Type: application/json" \
+--data '{"purge_everything":true}'

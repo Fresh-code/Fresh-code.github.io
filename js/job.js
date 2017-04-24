ef0878603e94d9a997c816345e320537888eccfc
Nikita <sirmonin@gmail.com>
2017-04-24 12:02:19 +0300

    Job form changed to Google Scripts

--- js/job.js
+++ js/job.js
@@ -4,36 +4,4 @@
  */
 (function(global) {
 
-
-    $('textarea.material-input').autogrow({ horizontal: false, flickering: false});
-    $(document).ready(function(){
-        $('.collapsible').collapsible({
-            accordion : false
-        });
-    });
-
-    var $form = $('form.job-form');
-
-    $form.parsley();
-    $form.submit(function (e) {
-        e.preventDefault();
-
-        var data = new FormData();
-        data.append('text', $form.find("textarea").val());
-        data.append('file', $('#file')[0].files[0]);
-
-        $.ajax({
-            method: "POST",
-            url: "https://getform.org/u/470a1fb1-94b3-4094-a7d7-565f28f0c877",
-            data: data,
-            processData: false,
-            contentType: false,
-            crossDomain: true
-        }).done(function(res) {
-            console.log(res);
-        });
-        $form[0].reset();
-
-//        setTimeout(window.location = '/', 5000);
-    });
 })(this);

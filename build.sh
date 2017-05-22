d1c88e065f6be4520695b7caca71edee50b8d1f3
Yevhenii Bilyk <blinkme1@ukr.net>
2017-05-22 12:49:32 +0300

    22/05/17 12:49:32

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ git commit -m "update site `date +"%d/%m/%y %H:%M:%S "`"
 git push origin master
 
 # Set vacancies position in iframe
-#curl -X POST -H "Content-Type:application/json" -d @./wp-data/iFramePositions.json https://script.google.com/macros/s/AKfycbyhbQjfhhooKVoDL9ftzK3PAcGZ0mLnAlhpa2m-W8t3nazT3qHs/exec
+curl -X POST -H "Content-Type:application/json" -d @./wp-data/iFramePositions.json https://script.google.com/macros/s/AKfycbyhbQjfhhooKVoDL9ftzK3PAcGZ0mLnAlhpa2m-W8t3nazT3qHs/exec
 
 # Purge all files in cloudflare
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/1c7a4eedbccff910e14ae06cc90a3622/purge_cache" \
